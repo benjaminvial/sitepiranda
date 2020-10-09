ad924a031650e8d415bcc008dc5605a3fd319217
benjaminvial <benjamin.vial@etu.univ-savoie.fr>
2020-10-09 14:02:43 +0200

    1er import du theme WP

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+<p> Fierement crée par moi </p>
+<?php wp_footer();?>
+</footer>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8"/>
+  <title><?php bloginfo('name');?>-<?php bloginfo('description');?></title>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" />
+<?php wp_head();?>
+</head>
+<header>
+  <nav id="menuprincipal">
+    <?php wp_page_menu('show_home=1');?>
+  <nav>
+  <aside>
+    <?php get_sidebar();?>
+  </aside>
+<h1> <?php bloginfo('name');?> </h1>
+<h2><?php bloginfo('description');?><h2>
+</header>
