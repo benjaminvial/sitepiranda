cdca91ec6504f3cf9702a0e29481bfaaa14c52ad
benjaminvial <benjamin.vial@etu.univ-savoie.fr>
2020-11-15 21:27:19 +0100

    retest

--- front_page.php
+++ front_page.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
 <section>
-  <h1>Ma page d'accueil statique</h1> <!--page statique que le client ne peux pas modifier-->
+  <h1>Ma page d'accueil statiqu e</h1> <!--page statique que le client ne peux pas modifier-->
 <section>
   <?php get_footer();?>
