9ce52a9eeb97f8e05501d6ba0ee21b7717d68e91
benjaminvial <benjamin.vial@etu.univ-savoie.fr>
2020-11-15 20:49:12 +0100

    commit

---
+++ front_page.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+<section>
+  <h1>Ma page d'accueil statique</h1> <!--page statique que le client ne peux pas modifier-->
+<section>
+  <?php get_footer();?>
---
+++ home.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+<section>
+  <!--The Loop (la boucle)-->
+<?php if( have_posts() ) :
+    while ( have_posts() ) :
+        the_post(); ?>
+<article>
+    <a href="<?php the_permalink();?>"><h2><?php the_title(); ?></h2></a>
+    <div><?php the_content(); ?></div>
+</article>
+<p> <?php comments_number();?>.</p>
+<?php endwhile;else:?><p> Désolé pas d'article!</p>
+ <?php endif; ?>
+<section>
+  <?php get_footer();?>
