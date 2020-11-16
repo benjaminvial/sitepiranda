530465d8432c43ad0510741a51fb734b6cbe6152
benjaminvial <benjamin.vial@etu.univ-savoie.fr>
2020-11-16 17:55:32 +0100

    ahah

--- header.php
+++ header.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>.
-<meta charset="utf-8"/>
-<?php wp_head();?>
+
+<html lang="fr" xmlns:og="http://ogp.me/ns#"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <meta name="author" content="Milan Comajuan">
+  <meta name="keywords" content="document html5 mmi">
+  <meta name="description" content="Document minimal html5 en DUT MMI">
+  <!-- Affichage standard sur périphérique mobile pour site adaptatif -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Données Open Graph -->
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="fr_FR">
+  <meta property="og:description" content="Document minimal html5 en DUT MMI">
+  <meta property="og:title" content="Document minimal en MMI">
+  <meta property="og:site_name" content="Document minimal en MMI">
+  <title>François Piranda</title>
+<link id="avast_os_ext_custom_font" href="css/accueil.css" rel="stylesheet" type="text/css">
+
 </head>
 <header>
   <nav id="menuprincipal">
     <ul id="menu">
-
 
 	    <img class="logopiranda" src="" alt="logo piranda" "width="" height="">
     	<a href="about.php" title="A propos de moi">A propos de moi</a>
@@ -28,3 +40,4 @@
 <h1> <?php bloginfo('name');?> </h1>
 <h2><?php bloginfo('description');?><h2>
 </header>
+<html>
