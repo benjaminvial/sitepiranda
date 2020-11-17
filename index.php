22ffd78a7e2d6b826182cae01f7b10b64ecf0381
roble2 <robin.meunierjacquingmail.com>
2020-11-17 14:24:35 +0100

    Ajouts de template

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <div class="wrap">
-    <div class="logotype">
-      <img src="" alt="Logo François Piranda">
-      <div class="social_media">
-        <img src="" alt="Logo Vimeo">
-        <img src="" alt="Logo Instagram">
-        <img src="" alt="Logo Pinterest">
-      </div>
-    </div>
-
-    <div>
-      <img src="" alt="Ecriture François Piranda">
-      <p>artiste</p>
-      <nav>
-        <a href="#">À propos de moi </a>
-        <a href="#">Inspirations</a>
-        <a href="#">Œuvres</a>
-        <a href="#">Contact</a>
-      </nav>
-    </div>
-  </div>
-</body>
-</html>
+<?php
+wp_redirect(site_url());
+exit;
+?>
---
+++ page-template/oeuvres.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Oeuvre
+ */
+get_header();?>
+<h1>Oeuvre</h1>
+<section>
+  <!--The Loop (la boucle)-->
+<?php if( have_posts() ) :
+    while ( have_posts() ) :
+        the_post(); ?>
+<article>
+    <h2><?php the_title(); ?></h2>
+    <div><?php the_content(); ?></div>
+</article>
+<?php endwhile;else:?>
+ <?php endif; ?>
+</section>
+  <?php get_footer();?>
