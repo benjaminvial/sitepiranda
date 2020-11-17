6e4c3fc28fa60e4c3adab4842b0dd90edb76d108
roble2 <robin.meunierjacquingmail.com>
2020-11-17 15:20:44 +0100

    link du css frontpage

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ function custom_theme_assets() {
     wp_enqueue_style('footer-style', get_template_directory_uri().'/css/footer.css');
     wp_enqueue_style('header-style', get_template_directory_uri().'/css/header.css');
     wp_enqueue_style('reset', get_template_directory_uri().'/css/reset.css');
-    wp_enqueue_style('index-style', get_template_directory_uri().'/css/index.css');
+    wp_enqueue_style('front-page-style', get_template_directory_uri().'/css/front-page.css');
 }
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets');
 
