d9686d02e2ca4500b796db9bac79b1f414d5629c
roble2 <robin.meunierjacquingmail.com>
2020-11-17 11:37:31 +0100

    link du css pour l'index

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@ function custom_theme_assets() {
     wp_enqueue_style('footer-style', get_template_directory_uri().'/css/footer.css');
     wp_enqueue_style('header-style', get_template_directory_uri().'/css/header.css');
     wp_enqueue_style('reset', get_template_directory_uri().'/css/reset.css');
+    wp_enqueue_style('index-style', get_template_directory_uri().'/css/index.css');
 }
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets');
 
