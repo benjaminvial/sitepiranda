ff41d320cfe5d4e69667796fa7ec5190749d611c
roble2 <robin.meunierjacquingmail.com>
2020-11-17 17:44:54 +0100

    f

--- front-page.php
+++ front-page.php
@@ -19,7 +19,7 @@
         <img src="<?php echo get_template_directory_uri();?>/pictures/logo/pinterest.svg" alt="Logo Pinterest">
       </div>
     </div>
-
+<!--ghjk-->
     <div>
       <img src="" alt="Ecriture François Piranda">
       <p>artiste</p>
