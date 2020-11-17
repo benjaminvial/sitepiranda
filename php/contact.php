76c7f11023a13752bcf0f250aeef63be7598527d
killianclaeyssen <killian.claeyssen@gmail.com>
2020-11-17 17:48:12 +0100

    formulaire 2.0

--- php/contact.php
+++ php/contact.php
@@ -21,14 +21,14 @@
 
 <div class="partie bleu">
   <form id="contact" method="post" action="traitement_formulaire.php">
-  <fieldset><legend>Vos coordonnées</legend>
+  <fieldset><legend> Coordonnées</legend>
     <p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" /></p>
     <p><label for="prenom">Prénom :</label><input type="text" id="prénom" name="prénom" /></p>
     <p><label for="email">Email :</label><input type="text" id="email" name="email" /></p>
+      <p><label for="phone">Téléphone :</label><input type="text" id="phone" name="phone" /></p>
   </fieldset>
 
-  <fieldset><legend>Votre message :</legend>
-    <p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" /></p>
+  <fieldset><legend>Message :</legend>
     <p><label for="message">Message :</label><textarea id="message" name="message" cols="30" rows="8"></textarea></p>
   </fieldset>
 
