4108dd10cd9dc74a84e6498317efc103fa0ec940
roble2 <robin.meunierjacquingmail.com>
2020-11-17 17:42:20 +0100

    avancement frontpage

--- front-page.php
+++ front-page.php
@@ -7,24 +7,16 @@
 <body>
   <div class="wrap">
     <div class="logotype">
-<<<<<<< HEAD
-      <img src="../pictures/logo/monogramme.svg" alt="Logo François Piranda">
-      <div class="social_media">
-        <img src="../pictures/logo/vimeo.svg" alt="Logo Vimeo">
-        <img src="../pictures/logo/insta.svg" alt="Logo Instagram">
-        <img src="../pictures/logo/pinterest.svg" alt="Logo Pinterest">
-=======
       <img src="<?php echo get_template_directory_uri();?>/pictures/logo/monogramme.svg" alt="Logo François Piranda">
       <div class="social_media">
         <img src="<?php echo get_template_directory_uri();?>/pictures/logo/vimeo.svg" alt="Logo Vimeo">
         <img src="<?php echo get_template_directory_uri();?>/pictures/logo/insta.svg" alt="Logo Instagram">
         <img src="<?php echo get_template_directory_uri();?>/pictures/logo/pinterest.svg" alt="Logo Pinterest">
->>>>>>> e969d1a5e29a88ab6eac63027f2df8c47a3a892b
       </div>
     </div>
 
-    <div>
-      <img src="" alt="Ecriture François Piranda">
+    <div class="title">
+      <img src="<?php echo get_template_directory_uri();?>/pictures/header/texte_front_page/francois piranda.svg" alt="Ecriture François Piranda">
       <p>artiste</p>
       <nav>
         <a href="#">À propos de moi </a>
