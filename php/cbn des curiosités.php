be5163c6d9e3949ae4e6bb5371b8c75b688c006b
killianclaeyssen <killian.claeyssen@gmail.com>
2020-11-18 11:43:39 +0100

    ajout dans les pages

--- php/cbn des curiosités.php
+++ php/cbn des curiosités.php
@@ -2,6 +2,17 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
+    <meta name="author" content="Milan Comajuan Killian claeyssen Benjamin Vial Robin Meunier">
+    <meta name="keywords" content="document html5 mmi">
+    <meta name="description" content="Document minimal html5 en DUT MMI">
+    <!-- Affichage standard sur périphérique mobile pour site adaptatif -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Données Open Graph -->
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="fr_FR">
+    <meta property="og:description" content="Document minimal html5 en DUT MMI">
+    <meta property="og:title" content="Document minimal en MMI">
+    <meta property="og:site_name" content="Document minimal en MMI">
   </head>
 <body>
 <div class="wrap">
