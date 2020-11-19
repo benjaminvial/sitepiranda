d15b7d54f17faa701784e24e1f5e423efdcd833b
benjaminvial <benjamin.vial@etu.univ-savoie.fr>
2020-11-19 09:18:41 +0100

    header php + css

--- header.php
+++ header.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 
-<html lang="fr" xmlns:og="http://ogp.me/ns#">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html lang="fr" xmlns:og="http://ogp.me/ns#"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-  <meta name="author" content="Milan Comajuan Killian claeyssen Benjamin Vial Robin Meunier">
+  <meta name="author" content="Milan Comajuan, Killian Claeyssen, Benjamin Vial, Robin Meunier">
   <meta name="keywords" content="document html5 mmi">
   <meta name="description" content="Document minimal html5 en DUT MMI">
   <!-- Affichage standard sur périphérique mobile pour site adaptatif -->
@@ -12,36 +10,33 @@
   <!-- Données Open Graph -->
   <meta property="og:type" content="website">
   <meta property="og:locale" content="fr_FR">
-  <meta property="og:description" content="Document minimal html5 en DUT MMI">
-  <meta property="og:title" content="Document minimal en MMI">
-  <meta property="og:site_name" content="Document minimal en MMI">
   <title>François Piranda</title>
-  <link id="avast_os_ext_custom_font" href="css/header.css" rel="stylesheet" type="text/css">
-  <link  href="css/footer.css" rel="stylesheet" type="text/css">
-  <link  href="css/reset.css" rel="stylesheet" type="text/css">
+<link id="avast_os_ext_custom_font" href="css/header.css" rel="stylesheet" type="text/css">
+<link  href="css/footer.css" rel="stylesheet" type="text/css">
+<link  href="css/reset.css" rel="stylesheet" type="text/css">
 
 <?php wp_head();?>
 
 </head>
+<header>
+<div class="top">
+  <div>
+	   <img src="<?php echo get_template_directory_uri();?>/pictures/logo/monogramme.svg" alt="logo piranda"  width=" " height=" ">
+  </div>
+      <div>
+    	     <a href="about.php"  title="A propos de moi">À propos de moi</a>
+    	     <a href="inspiration.php" title="Mes inspirations">Inspirations</a>
+    	     <a href="oeuvres.php" title="Mes oeuvres">Œuvres</a>
+    	     <a href="contact.php" title="contact">Contact</a>
+      </div>
 
-  <?php	$imageBanner = get_field('bannerHeader');
-      if( !empty($imageBanner) ):?>
-      <header style="background-image: url(<?php echo $imageBanner['url']; ?>);">
-  <?php endif; ?>
-
-	    <img class="logopiranda" src="<?php echo get_template_directory_uri();?>/pictures/logo/monogramme.svg" alt="logo piranda"  width=" " height=" ">
-		<div class="a">
-    	<a  href="about.php" title="A propos de moi">À propos de moi</a>
-    	<a  href="inspiration.php" title="Mes inspirations">Inspirations</a>
-    	<a  href="oeuvres.php" title="Mes oeuvres">Œuvres</a>
-    	<a href="contact.php" title="contact">Contact</a>
-		</div>
-      <img class="logoa" src="<?php echo get_template_directory_uri();?>/pictures/logo/vimeo.svg" alt="logo vimeo" width="30" height="30">
-      <img class="logob" src="<?php echo get_template_directory_uri();?>/pictures/logo/insta.svg" alt="logo instagram" width="30" height="30">
-      <img class="logoc" src="<?php echo get_template_directory_uri();?>/pictures/logo/pinterest.svg" alt="logo pinterest" width="30" height="30">
-
-<img class="ecriture" src="pictures/header/Apropos/Titre.svg" alt="ecriture a propos" width="600" height="600">
-
+      <div>
+		      <img  src="<?php echo get_template_directory_uri();?>/pictures/logo/vimeo.svg" alt="logo vimeo" width="30" height="30">
+          <img  src="<?php echo get_template_directory_uri();?>/pictures/logo/insta.svg" width="30" height="30">
+		      <img  src="<?php echo get_template_directory_uri();?>/pictures/logo/pinterest.svg" width="30" height="30">
+     </div>
+</div>
+      <img src="<?php echo get_template_directory_uri();?>/pictures/header/Apropos/titre.svg" alt="ecriture a propos" width="600" height="600">
+</header>
 <h1> <?php bloginfo('name');?> </h1>
 <h2><?php bloginfo('description');?><h2>
-</header>
