ac13a790eab7109f3bd09deb205349f34185bf92
killianclaeyssen <killian.claeyssen@gmail.com>
2020-11-19 11:18:24 +0100

    contact page

--- page-contact.php
+++ page-contact.php
@@ -1,26 +1,54 @@
-<?php get_header();?>
+<!DOCTYPE HTML>
+<head>
+
+
+<link rel="stylesheet" type="text/css" href="/contact.css">
+
+</head>
 <body>
 <div class="wrap">
 
-<div class="partie blanche">
+<div class="blanche">
+
+    <div class="infos">
   <!--ici nous avons la partie blanche de la page contact-->
   <h1> Me contacter </h1>
-    <p> Pour toute information sur mes oeuvres et </p>
-      <br>
-    <p>mon travail.</p>
-  <img src="<?php echo get_template_directory_uri();?>/pictures/logo/logo_bleu/phone.svg" alt="logo phone" width="" height="">
-  <h2> 06 77 65 97 77 </h2>
-  <img src="<?php echo get_template_directory_uri();?>/pictures/logo/logo_bleu/@.svg" alt="logo email" width="" height="">
-  <h2> contact@francois-piranda.art </h2>
-      <img src="<?php echo get_template_directory_uri();?>/pictures/logo/logo_bleu/instagram.svg" alt="logo instagram" width="" height="">
-      <!-- ici il y aura l'insta-->
-        <img src="<?php echo get_template_directory_uri();?>/pictures/logo/logo_bleu/viméo.svg" alt="logo vimeo" width="" height="">
-        <!-- ici il y aura le viméo-->
-          <img src="<?php echo get_template_directory_uri();?>/pictures/logo/logo_bleu/pinterest.svg" alt="logo pinterest" width="" height="">
-          <!-- ici il y aura le pinterest-->
+    <span id="gg"> Pour toute information sur mes oeuvres et<br>
+    mon travail.</span>
+
+  <div class="info">
+    <img src="logo/logo_bleu/phone.svg" alt="logo phone" width="" height="">
+      <span> 06 77 65 97 77 </span>
 </div>
+
+
+<div class="info">
+  <img src="logo/logo_bleu/email.png" alt="logo email" width="" height="">
+  <span id="rs"> contact@francois-piranda.art </span>
+</div>
+
+<div class="info">
+   <img src="logo/logo_bleu/instagram.svg" alt="logo instagram" width="" height="">
+   <span> instagram </span>
+</div>
+
+
+<div class="info">
+    <img  src="logo/logo_bleu/viméo.svg" alt="logo vimeo" width="" height="">
+   <span> viméo </span>
+</div>
+
+
+   <div class="info">
+     <img src="logo/logo_bleu/pinterest.svg">
+     <span> pinterest </span>
+</div>
+</div>
+</div>
+
+
 <!-- ici nous avons la partie bleu de la page contact-->
-<div class="partie bleu">
+<div id="bleu">
   <form id="contact" method="post" action="traitement_formulaire.php">
   <fieldset><legend> Coordonnées</legend>
     <p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" /></p>
@@ -30,12 +58,12 @@
   </fieldset>
 
   <fieldset><legend>Message :</legend>
-    <p><label for="message">Message :</label><textarea id="message" name="message" cols="30" rows="8"></textarea></p>
+    <p><label for="message"></label><textarea id="message" name="message" cols="30" rows="8"></textarea></p>
   </fieldset>
   <br>
-  <p id=politique> En envoyant ce message vous consentez à la collecte et au traitement des
-    données renseignées ci-dessus pour l'usage exclusif.<p>
-  <p> <blod> En savoir plus </bold> </p>
+  <span id="politique"> En envoyant ce message vous consentez à la collecte et au traitement des
+    données renseignées ci-dessus pour l'usage exclusif.</span>
+  <span id="esp"> <bold> En savoir plus</bold> </span>
 
 
   <div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer" /></div>
@@ -45,4 +73,3 @@
 </div>
 
   </body>
-  <?php get_footer();?>
